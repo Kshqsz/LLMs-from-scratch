be7cbdaebee15f1e6bf883bf176814c6b3c74243
2456480538@qq.com <2456480538@qq.com>
2025-12-28 13:30:06 +0800

    implement the calc about loss

--- ch05_pretraining_on_unLabeledData/pretraining.py
+++ ch05_pretraining_on_unLabeledData/pretraining.py
@@ -45,50 +45,7 @@ else:
     with open(file_path, "r", encoding="utf-8") as file:
         text_data = file.read()
 
-print(text_data[:99])
-
-total_characters = len(text_data)
-total_tokens = len(tokenizer.encode(text_data))
-
-print(total_characters)
-print(total_tokens)
-
-train_ratio = 0.90
-split_idx = int(train_ratio * len(text_data))
-train_data = text_data[:split_idx]
-val_data = text_data[split_idx:]
-
-print(f"train_data : {len(train_data)}")
-print(f"val_data : {len(val_data)}")
-
-torch.manual_seed(123)
-
-train_loader = create_dataloader_v1(
-    txt = train_data,
-    batch_size = 2,
-    max_length = GPT_CONFIG_124M["context_length"],
-    stride = GPT_CONFIG_124M["context_length"],
-    shuffle = True,
-    drop_last = True
-)
-
-val_loader = create_dataloader_v1(
-    txt = val_data,
-    batch_size = 2,
-    max_length = GPT_CONFIG_124M["context_length"],
-    stride = GPT_CONFIG_124M["context_length"],
-    shuffle = True,
-    drop_last = True
-)
-
-for input_batch, target_batch in train_loader:
-    pass
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-
-model.to(device)
-print(len(train_loader))
 
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch, target_batch = input_batch.to(device), target_batch.to(device)
@@ -113,12 +70,56 @@ def calc_loss_loader(data_loader, model, device, num_batchs = None):
     return total_loss / num_batchs
 
 
+def train_model_simple(model, train_loader, val_loader, optimizier, device, num_epochs,
+                       eval_freq, eval_iter, start_context, tokenizer):
+    
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
 
-torch.manual_seed(123)
+    for epoch in range(num_epochs):
+        model.train()
 
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, device)
-    val_loss = calc_loss_loader(val_loader, model, device)
+        for input_batch, target_batch in train_loader:
+            optimizier.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizier.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
 
-print(train_loss)
-print(val_loss)
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch + 1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batchs = eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batchs = eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model = model, idx = encoded,
+            max_new_tokens = 50, context_size = context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+print(device)
+
