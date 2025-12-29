d69bd4225a92885261dd1d1d43190012b224fa18
2456480538@qq.com <2456480538@qq.com>
2025-12-29 15:32:00 +0800

    implement the dataloder for ch06

--- ch06_finetuning_for_text_classification/main.py
+++ ch06_finetuning_for_text_classification/main.py
@@ -44,7 +44,7 @@ except (requests.exceptions.RequestException, TimeoutError) as e:
 import pandas as pd
 
 df = pd.read_csv(data_file_path, sep = "\t", header = None, names = ["Label", "Text"])
-print(df)
+# print(df)
 
 
 def create_balanced_dataset(df):
@@ -54,13 +54,13 @@ def create_balanced_dataset(df):
     return balanced_df
 
 balanced_df = create_balanced_dataset(df)
-print(balanced_df["Label"].value_counts())
+# print(balanced_df["Label"].value_counts())
 
 map_dict = {"ham" : 0, "spam" : 1}
 balanced_df["Label"] = balanced_df["Label"].map(map_dict)
-print(balanced_df.head())
+# print(balanced_df.head())
 
-print(balanced_df.tail())
+# print(balanced_df.tail())
 
 def random_split(df, train_frac = 0.7, validation_frac = 0.2):
     df = df.sample(frac = 1, random_state = 1).reset_index(drop = True)
@@ -76,14 +76,114 @@ def random_split(df, train_frac = 0.7, validation_frac = 0.2):
 train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
 
 train_df.to_csv("train.csv", index = None)
-validation_df.to_csv("valitation.csv", index = None)
+validation_df.to_csv("validation.csv", index = None)
 test_df.to_csv("test.csv", index = None)
 
-print(train_df)
-print()
+# print(train_df)
+# print()
 
-print(validation_df)
-print()
+# print(validation_df)
+# print()
 
-print(test_df)
-print()
+# print(test_df)
+# print()
+
+# 6.3 Creating data loaders
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+print(tokenizer.decode([50256]))
+
+import torch
+from torch.utils.data import Dataset
+
+class SpamDataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length = None, pad_token_id = 50256):
+        self.data = pd.read_csv(csv_file)
+
+        self.encoded_texts = [
+            tokenizer.encode(text) for text in self.data["Text"]
+        ]
+
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [
+                encoded_text[:max_length]
+                for encoded_text in self.encoded_texts
+            ]
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+    def __getitem__(self, index):
+        encoded = self.encoded_texts[index]
+        label = self.data.iloc[index]["Label"]
+        return (
+            torch.tensor(encoded, dtype = torch.long),
+            torch.tensor(label, dtype= torch.long)
+        )
+    def __len__(self):
+        return len(self.data)
+
+    def _longest_encoded_length(self):
+        return max(len(encoded_text) for encoded_text in self.encoded_texts)
+
+train_dataset = SpamDataset(
+    csv_file = "train.csv",
+    max_length = None,
+    tokenizer = tokenizer
+)
+
+print(train_dataset.max_length)
+val_dataset = SpamDataset(
+    csv_file = "validation.csv",
+    max_length = train_dataset.max_length,
+    tokenizer = tokenizer
+)
+test_dataset = SpamDataset(
+    csv_file = "test.csv",
+    max_length = train_dataset.max_length,
+    tokenizer = tokenizer
+)
+
+from torch.utils.data import DataLoader
+
+num_workers = 0
+batch_size = 8
+
+torch.manual_seed(123)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    shuffle=True,
+    drop_last=True
+)
+
+val_loader = DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False
+)
+
+print("Train loader:")
+for input_batch, target_batch in train_loader:
+    pass
+
+print("Input batch dimensions:", input_batch.shape)
+print("Label batch dimensions", target_batch.shape)
+
+print(f"{len(train_loader)} training batches")
+print(f"{len(val_loader)} validation batches")
+print(f"{len(test_loader)} test batches")
